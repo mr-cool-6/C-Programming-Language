60ffc1a290a21dec0b37ed8efde2fe2219959c69
mr-cool-6 <62960243+mr-cool-6@users.noreply.github.com>
2020-10-17 00:32:16 +0530

    Add files via upload

---
+++ C Programming Language/Elements in C programming Language.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main ()
+{
+  printf ("Hello World");
+  return 0;
+}
+
+ /*                 -  Comment line start. 
+ #                  -  Preprocessor directive.
+ include            -  To combine. 
+ <>                 -  Go and search of the file in standard folder.
+ stdio.h            -  Header file, function prototype --> Macros.
+ int                -  Data type, we have other data types also.
+ main               -  Start the program excution. 
+ ( )                -  To separate a piece of logic
+ { }                -  Block. 
+ print              -  Library to print.
+ f                  -  Functoion. 
+ " "                -  Go and search for current folder.
+ return             -  Ends excution and returns to control to calling function.  
+ 0                  -  Without any integer.
+ ;                  -  It tells that the current statement has been terminated and other statements following are new statements.
+ "stdio.h"          -  Instard of <stdio.h> -> also get result. 
+ Comment line ends  -  */
