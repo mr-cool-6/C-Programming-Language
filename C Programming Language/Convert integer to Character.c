5332c21e66a4744a5f40199d95ff08aee31da464
mr-cool-6 <62960243+mr-cool-6@users.noreply.github.com>
2020-10-17 01:23:02 +0530

    int - char

---
+++ C Programming Language/Convert integer to Character.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int Alphabet;
+    printf("Enter alphabet: ");
+    scanf("%c",&Alphabet);
+	
+	char value;
+	value = Alphabet;
+	printf("%c,%d",value, value);
+	return 0;
+	
+}
