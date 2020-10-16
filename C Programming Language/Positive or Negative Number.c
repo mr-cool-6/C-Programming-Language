73b3b5646ef38207cf28ca84d50f15163e96d3c7
mr-cool-6 <62960243+mr-cool-6@users.noreply.github.com>
2020-10-17 01:03:27 +0530

    +ve or -ve number

---
+++ C Programming Language/Positive or Negative Number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{ 
+    int a;
+    printf("Enter a number");
+    scanf("%d",&a);
+    if (a>0)
+    printf("number is +ve",a);
+    else
+    printf("number is -ve",a);
+    return 0;
+}
